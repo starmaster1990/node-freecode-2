dbefbcb226cb3178ba5819557c5826e46302d8a8
starmaster1990 <ronny.tigrero.22@gmail.com>
2021-04-18 11:25:47 -0500

    devolviendo json

--- myApp.js
+++ myApp.js
@@ -6,7 +6,12 @@ const path = `${__dirname}/views/index.html`
 app.use('/public',express.static(`${__dirname}/public`))
 
 app.get('/',(req, res) =>{
-    res.sendFile(path);
+    res.sendFile(path)
 })
 
+app.get('/json', (req, res) => {
+    const miObjeto = {"message": "Hello json"}
+    res.setHeader('Content-Type', 'application/json');
+    res.json(miObjeto)
+})
 module.exports = app;
