26ccf6a09c18a1b0c2a861cd501a76fd32ab6280
starmaster1990 <ronny.tigrero.22@gmail.com>
2021-04-18 10:48:16 -0500

    devolviendo vista

--- myApp.js
+++ myApp.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 
+const path = `${__dirname}/views/index.html`
 app.get('/',(req, res) =>{
-    res.send('Hello Express')
+    res.sendFile(path);
 })
 module.exports = app;
