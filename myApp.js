29cf539967de018ac4e7b29542fbf967e8a96785
starmaster1990 <ronny.tigrero.22@gmail.com>
2021-04-18 11:13:08 -0500

    agregando middleware static haciendo referencia a la carpeta public

--- myApp.js
+++ myApp.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 
 const path = `${__dirname}/views/index.html`
+
+app.use('/public',express.static(`${__dirname}/public`))
+
 app.get('/',(req, res) =>{
     res.sendFile(path);
 })
+
 module.exports = app;
