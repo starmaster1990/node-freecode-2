b10c7fc2fdbb22950f80289e059367db1a2542e9
starmaster1990 <ronny.tigrero.22@gmail.com>
2021-04-18 19:17:28 -0500

    end point con parametro

--- myApp.js
+++ myApp.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app     = express();
 var env     = require("dotenv").config();
 
-const path = `${__dirname}/views/index.html`
 
 app.use('/public',express.static(`${__dirname}/public`))
 
@@ -11,6 +10,7 @@ app.use((req, res, next) => {
     next()
 })
 
+const path = `${__dirname}/views/index.html`
 app.get('/',(req, res) =>{
     res.sendFile(path)
 })
